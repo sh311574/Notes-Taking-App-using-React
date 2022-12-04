9aac0c447f8d014cbf45dde222b09e8b102fa021
sh311574 <81141918+sh311574@users.noreply.github.com>
2022-12-04 19:39:48 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Notes from "./Notes";
+function App() {
+  return (
+    <div className="note">
+      <Header />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Notes() {
+  return (
+    <p>
+      Thanks Mentor for teaching me React. This help me in start working in
+      React field as a beginner. Thanks again.
+    </p>
+  );
+}
+export default Notes;
